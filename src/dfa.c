4cf841c71151a46e3f18a178e0739e7267b3f7d2
ElizavetaMenshikova <mefmag01@gmail.com>
2026-05-01 00:36:27 +0300

    После команды clang-format -i src/*.c src/*.h получились отступы по 2 пробела, исправила на 4

--- src/dfa.c
+++ src/dfa.c
@@ -1,33 +1,33 @@
 #include "dfa.h"
 
 bool dfaCheck(const Dfa *dfa, const char *input, DfaStatus *status) {
-  int currentState = dfa->startState;
-  *status = DfaOk;
+    int currentState = dfa->startState;
+    *status = DfaOk;
 
-  for (int i = 0; input[i] != '\0'; i++) {
-    char c = input[i];
-    bool found = false;
+    for (int i = 0; input[i] != '\0'; i++) {
+        char c = input[i];
+        bool found = false;
 
-    for (int j = 0; j < dfa->numTransitions; j++) {
-      if (dfa->transitions[j].from == currentState &&
-          dfa->transitions[j].symbol == c) {
-        currentState = dfa->transitions[j].to;
-        found = true;
-        break;
-      }
+        for (int j = 0; j < dfa->numTransitions; j++) {
+            if (dfa->transitions[j].from == currentState &&
+                dfa->transitions[j].symbol == c) {
+                currentState = dfa->transitions[j].to;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            *status = DfaErrInvalidChar;
+            return false;
+        }
     }
 
-    if (!found) {
-      *status = DfaErrInvalidChar;
-      return false;
+    for (int i = 0; i < dfa->numAcceptStates; i++) {
+        if (currentState == dfa->acceptStates[i]) {
+            return true;
+        }
     }
-  }
 
-  for (int i = 0; i < dfa->numAcceptStates; i++) {
-    if (currentState == dfa->acceptStates[i]) {
-      return true;
-    }
-  }
-
-  return false;
+    return false;
 }
